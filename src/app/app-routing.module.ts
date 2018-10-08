1e36c8e0667ece41976520cc543a755354105d95
Prakash <prakashdas1998.pd@gmail.com>
2018-10-08 13:35:56 +0530

    Modified many files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import { Route } from '@angular/compiler/src/core';
-import { PostListComponent } from './posts/post-list/post-list.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes : Routes = [
-    { path: '', component: PostListComponent},
-    {path: 'create', component: PostCreateComponent},
-    {path: 'edit/:postId', component: PostCreateComponent}
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { LoginComponent } from "../../../../.Trash/src/app/auth/login/login.component";
+import { SignupComponent } from "../../../../.Trash/src/app/auth/signup/signup.component";
+import { AuthGuard } from "../../../../.Trash/src/app/auth/auth.guard";
+
+const routes: Routes = [
+  { path: "", component: PostListComponent },
+  { path: "create", component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: "edit/:postId", component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
-
 export class AppRoutingModule {}
